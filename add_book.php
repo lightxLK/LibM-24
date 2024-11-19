a8558851183e59f8ac753d8e21e6f3c28df8aa30
Lokesh Patra <lokesh0patra@gmail.com>
2024-11-19 14:42:25 +0530

    v5 Favicon'd

--- add_book.php
+++ add_book.php
@@ -58,6 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" type="image/x-icon" href="favicon\android-chrome-512x512.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Book</title>
